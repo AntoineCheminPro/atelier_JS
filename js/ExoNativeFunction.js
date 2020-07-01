9962a41f53ac930fa5acd28208a1203daa0dd722
Antoine Chemin <antoine.chemin.pro@outlook.fr>
2020-07-01 16:48:33 +0200

    exercices fonctions finis

---
+++ js/ExoNativeFunction.js
@@ -0,0 +1,19 @@
+console.log("Exercices fonctions");
+// exercice 1
+console.log("Exercice 1");
+
+var aleatoire = Math.random();
+// nothing as we asked
+// this function give a random number between 0 and 1 (1 off)
+// permet de créer des nombres aléatoirement, pour un jeu de dés par exemple
+
+console.log(aleatoire);
+
+
+// Exercice 2
+console.log("exercice 2");
+number =12.42359;
+console.log(number);
+// numérique
+
+
