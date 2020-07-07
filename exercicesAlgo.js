f6d354c180eb4a37aebe1306e865108a6985936a
Antoine Chemin <antoine.chemin.pro@outlook.fr>
2020-07-07 17:05:39 +0200

    exercice 9 en cours

--- exercicesAlgo.js
+++ exercicesAlgo.js
@@ -47,28 +47,79 @@
 // }
 
 
-console.log("exercice 3");
+// console.log("exercice 3");
+// console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+// // exercice 3
+
+// var secretNumber = 20;
+// var userChoice = null;
+// while (userChoice !== secretNumber){
+//     userChoice = parseInt( prompt ("quel nombre proposes-tu?"));
+//     if (userChoice > secretNumber){
+//         alert ("c'est moins");
+//     }
+//     else if(userChoice < secretNumber) {
+//         alert ( "c'est plus");
+//     }
+// }
+// alert ("Bravo !");
+
+// console.log("exercice 4");
+// console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+// // exercice 4
+// for (i=1; i<= 100; i++ ){
+//     console.log(i);
+// }
+
+// console.log("exercice 5");
+// console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+// // exercice 5
+// for (i=1; i<= 100; i++ ){
+//     if ( i%2 === 0) {
+//         console.log(i);
+//     }
+// }
+// console.log("exercice 6");
+// console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+// // exercice 6
+// var debit = prompt ("A quel débit remplit-on la piscine?");
+// var longueur = 10;
+// var largeur = 20;
+// var profondeur = 40;
+// function tempsRemplissage (longueur, largeur, profondeur, debit){
+//     let volume = longueur * largeur * profondeur;
+//     let temps = volume / debit;
+//     return temps;
+// }
+
+// let temps = tempsRemplissage(longueur, largeur, profondeur, debit);
+// console.log(`Il faudra ${temps} minutes`);
+
+// // console.log("exercice 8");
+// console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+// // exercice 8
+// let star ="";
+// for (i = 0 ; i <= 5; i++){
+//     star += "*";
+//     console.log(star);
+// }
+console.log("exercice 9");
 console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-// exercice 3
-
-var userChoice = "";
-var secretNumber = Math.round (Math.random() * 20);
-function test(userChoice){
-    if (secretNumber === userChoice){
-            answer = "Bravo !";
+// exercice 9
+for (i=1; i<= 100; i++ ){
+    let show ="";
+    if (i%15 === 0)
+    {
+       show = (i + " FIZZBUZZ");
     }
-    else if (secretNumber < userChoice){
-            answer = "c'est moins";
+    else if (i%5 === 0)
+    {
+        show =(i + " BUZZ");
     }
-    answer = "c'est plus"
-return answer;
-};
-
-// choose a secret number 0 to 20
-
-console.log(secretNumber);
-while (secretNumber !== userChoice){
-userChoice = parseInt (prompt (test(userChoice) + "\n quel nombre proposes-tu?"));
+    else if (i%3 === 0)
+    {
+        show =(i + " FIZZ");
+    }
+    show =i;
+console.log(show);
 }
-
-
